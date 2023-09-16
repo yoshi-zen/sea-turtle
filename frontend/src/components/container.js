6f75c324fb8dfc7dcf1db0ab32b054ae28aa9540
yoshi-zen <yoshi180705@ruri.waseda.jp>
2023-09-16 14:37:00 +0900

    UIもろもろの構築をした

---
+++ frontend/src/components/container.js
@@ -0,0 +1,5 @@
+import styles from '@/styles/container.module.css';
+
+export default function Container({ children, large = false }) {
+  return <div className={large ? styles.large : styles.default}>{children}</div>;
+}
---
+++ frontend/src/components/header.js
@@ -0,0 +1,22 @@
+import styles from '@/styles/header.module.css';
+import Container from './container';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Container large>
+        <div className={styles.flexContainer}>
+          <div className={styles.header_title}>
+            <h1>ウミガメのスープ</h1>
+            <p>水平思考の間</p>
+          </div>
+          <ul className={styles.header_menu}>
+            <li>ホーム</li>
+            <li>問題一覧</li>
+            <li>マイページ</li>
+          </ul>
+        </div>
+      </Container>
+    </header>
+  );
+}
