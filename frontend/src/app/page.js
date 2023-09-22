db4156038cf4a6d0d7236f26e704e45cb3aab1c7
yoshi-zen <yoshi180705@ruri.waseda.jp>
2023-09-22 20:05:33 +0900

    トップページのアイキャッチUI完成

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -9,9 +9,10 @@ export default function Home() {
       <div className={styles.hero_container}>
         <div className={styles.heroText_wrapper}>
           <h1>水平思考の間へようこそ！</h1>
+          <p>「はい」か「いいえ」の質問をして、答を導け！</p>
         </div>
         <div className={styles.heroImg_wrapper}>
-          <Image src={turtleImg} alt='turtle' width={500} priority />
+          <Image src={turtleImg} alt='turtle' layout='responsive' priority />
         </div>
       </div>
     </>
