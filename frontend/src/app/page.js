be716ecc3efdf79916d4fad7e7151875df2580e3
yoshi-zen <yoshi180705@ruri.waseda.jp>
2023-09-22 18:36:43 +0900

    トップページの頭のUIを部分構築

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,8 +6,14 @@ import turtleImg from 'public/turtle01.jpg';
 export default function Home() {
   return (
     <>
-      <h1>水平思考の間へようこそ！</h1>
-      <Image src={turtleImg} alt='turtle' />
+      <div className={styles.hero_container}>
+        <div className={styles.heroText_wrapper}>
+          <h1>水平思考の間へようこそ！</h1>
+        </div>
+        <div className={styles.heroImg_wrapper}>
+          <Image src={turtleImg} alt='turtle' width={500} priority />
+        </div>
+      </div>
     </>
   );
 }
