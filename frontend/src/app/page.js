58100d492c980cd88f3aeeed5c54eca5cf1a6aca
yoshi-zen <yoshi180705@ruri.waseda.jp>
2023-09-22 17:23:43 +0900

    global.cssをいじった

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 
+import turtleImg from 'public/turtle01.jpg';
+
 export default function Home() {
   return (
     <>
       <h1>水平思考の間へようこそ！</h1>
+      <Image src={turtleImg} alt='turtle' />
     </>
   );
 }
