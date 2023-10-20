1ceebba3c910dd6ca32be211c5e7cb064be5c37a
yoshi_zen <yoshi180705@gmail.com>
2023-10-20 12:22:11 +0900

    レスポンシブ対応で不調だったところを修正

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,19 +1,23 @@
+'use client';
+
 import styles from '@/styles/header.module.css';
 import Container from './container';
+import { useRouter } from 'next/navigation';
 
 export default function Header() {
+  const router = useRouter();
   return (
     <header className={styles.header}>
       <Container large>
         <div className={styles.flexContainer}>
-          <div className={styles.header_title}>
-            <h1>ウミガメのスープ</h1>
-            <p>水平思考の間</p>
+          <div className={styles.header_title} onClick={() => router.push('/')}>
+            <h2>ウミガメのスープ</h2>
+            <p>～水平思考の間～</p>
           </div>
           <ul className={styles.header_menu}>
-            <li>ホーム</li>
-            <li>問題一覧</li>
-            <li>log in</li>
+            <li onClick={() => router.push('/')}>ホーム</li>
+            <li onClick={() => router.push('/questions')}>問題一覧</li>
+            <li onClick={() => router.push('/')}>log in</li>
           </ul>
         </div>
       </Container>
