20bb76f1e47fb1db940b147ce2f71239aacc9189
Exzrgs <s.nakao0120@gmail.com>
2023-10-25 17:16:52 +0900

    問題を追加するAPIを使用した際、問題文が正しく反映されないバグを修正

--- backend/models/models.go
+++ backend/models/models.go
@@ -3,16 +3,16 @@ package models
 import "time"
 
 type Problem struct {
-	ID               int       `json: "id"`
-	Title            string    `json: "title"`
-	ProblemStatement string    `json: "problem_statement"`
-	Answer           string    `json: "answer"`
-	CreatedAt        time.Time `json: "created_at"`
+	ID               int       `json:"id"`
+	Title            string    `json:"title"`
+	ProblemStatement string    `json:"problem_statement"`
+	Answer           string    `json:"answer"`
+	CreatedAt        time.Time `json:"created_at"`
 }
 
 type User struct {
-	ID         int       `json: "id"`
-	Name       string    `json: "name"`
-	SolvedList []int     `json: "solved_list"`
-	CreatedAt  time.Time `json: "created_at"`
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	SolvedList []int     `json:"solved_list"`
+	CreatedAt  time.Time `json:"created_at"`
 }
