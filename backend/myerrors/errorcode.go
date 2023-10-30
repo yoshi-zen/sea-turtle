b82d1a62a00c9acf857732d90747850628ad4740
Exzrgs <s.nakao0120@gmail.com>
2023-10-31 00:15:43 +0900

    エラーコードを調整

--- backend/myerrors/errorcode.go
+++ backend/myerrors/errorcode.go
@@ -5,11 +5,10 @@ type ErrCode string
 const (
 	Unknown ErrCode = "U000"
 
-	DBInsertFailed ErrCode = "D000"
-	DBSelectFailed ErrCode = "D001"
+	InsertDataFailed ErrCode = "D000"
+	GetDataFailed    ErrCode = "D001"
 
-	RowScanFailed ErrCode = "S000"
-	NoData        ErrCode = "S002"
+	NoData ErrCode = "S000"
 
 	ReqDecodeFailed ErrCode = "R000"
 	BadParameter    ErrCode = "R001"
