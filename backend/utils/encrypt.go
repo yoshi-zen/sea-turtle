8cec1c0755f70d6bb41a7b5f22f8cb79f6d4348c
Exzrgs <s.nakao0120@gmail.com>
2023-11-06 18:33:42 +0900

    ハッシュ化の関数を実装

---
+++ backend/utils/encrypt.go
@@ -0,0 +1,10 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func PasswordEncrypt(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	return string(hash), err
+}
